675a886309e2d190a2a9e1927e84e822e180b861
wangxianpeng <wangxianpeng@jd.com>
2015-08-27 15:45:47 +0800

    day7

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ CustomView Day1
 
 * 自定义View实现点击显示随机四位数
 * 参考网址：http://blog.csdn.net/lmj623565791/article/details/24252901
+* Created by wxp on 2015/8/17.
 
 效果图：
 
@@ -15,6 +16,7 @@ CustomView Day2
 
 * 自定义View实现图片与文字上下并排
 * 参考网址：http://blog.csdn.net/lmj623565791/article/details/24300125
+* Created by wxp on 2015/8/17.
 
 效果图：
 
@@ -26,6 +28,7 @@ CustomView Day3
 
 * 自定义View实现进度圈
 * 参考网址：http://blog.csdn.net/lmj623565791/article/details/24500107
+* Created by wxp on 2015/8/19.
 
 效果图：
 
@@ -37,6 +40,7 @@ CustomView Day4
 
 * 自定义View实现音量圈以及加减效果
 * 参考网址：http://blog.csdn.net/lmj623565791/article/details/24529807
+* Created by wxp on 2015/8/19.
 
 效果图：
 
@@ -48,6 +52,7 @@ CustomView Day5
 
 * 自定义ViewGroup实现四个子view分别位于四个角落
 * 参考网址：http://blog.csdn.net/lmj623565791/article/details/38339817
+* Created by wxp on 2015/8/25.
 
 效果图：
 
@@ -59,7 +64,18 @@ CustomView Day6
 
 * 自定义View实现FlowLayout
 * 参考网址：http://blog.csdn.net/lmj623565791/article/details/38352503
+* Created by wxp on 2015/8/27.
 
 效果图：
 
 ![/imgs/day6.png](/imgs/day6.png)
+
+- - -
+
+CustomView Day7
+
+* 自定义View实现圆角矩形图像以及圆形图像
+* 参考网址：http://blog.csdn.net/lmj623565791/article/details/24555655
+* Created by wxp on 2015/8/27.
+
+![/imgs/day7.png](/imgs/day7.png)
---
+++ app/src/main/java/com/wxp/customview/Day7View.java
@@ -0,0 +1,148 @@
+package com.wxp.customview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+
+/**
+ * CustomView Day7
+ * 自定义View实现圆角矩形图像以及圆形图像
+ * 参考网址：http://blog.csdn.net/lmj623565791/article/details/24555655
+ * Created by wxp on 2015/8/27.
+ */
+public class Day7View extends View {
+    Bitmap mSrcBmp = null;
+    int mSrcType = 0;
+    int mCornerRadius = 10;
+    Paint mPaint = null;
+
+    public Day7View(Context context) {
+        this(context, null, 0);
+    }
+
+    public Day7View(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public Day7View(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.Day7View, defStyleAttr, 0);
+        int length = array.length();
+        for (int i = 0; i < length; i++) {
+            int attr = array.getIndex(i);
+            switch (attr) {
+                case R.styleable.Day7View_src:
+                    mSrcBmp = BitmapFactory.decodeResource(getResources(), R.drawable.whisper);
+                    break;
+
+                case R.styleable.Day7View_type:
+                    mSrcType = array.getInt(attr, 0);
+                    break;
+
+                case R.styleable.Day7View_cornerRadius:
+                    mCornerRadius = (int) array.getDimension(attr, TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics()));
+                    break;
+            }
+        }
+        array.recycle();
+
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        int width = 0;
+        int height = 0;
+        if (widthMode == MeasureSpec.EXACTLY) {
+            width = widthSize;
+        } else {
+            /**
+             * Point1　：　注意计算宽度时要包含padding值
+             */
+            width = mSrcBmp.getWidth() + getPaddingLeft() + getPaddingRight();
+        }
+
+        if (heightMode == MeasureSpec.EXACTLY) {
+            height = heightSize;
+        } else {
+            height = mSrcBmp.getHeight() + getPaddingTop() + getPaddingTop();
+        }
+
+        setMeasuredDimension(width, height);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        mSrcBmp = Bitmap.createScaledBitmap(mSrcBmp, getWidth(), getHeight(), false);
+        /**
+         * Point２　：　根据不同类型绘制不同图像
+         */
+        if (mSrcType == 0) {
+            canvas.drawBitmap(createCircleBitmap(mSrcBmp, Math.min(getWidth(),getHeight())), 0, 0, mPaint);
+        } else {
+            canvas.drawBitmap(createRoundCornerBitmap(mSrcBmp, getWidth(), getHeight()), 0, 0, mPaint);
+        }
+    }
+
+    /**
+     * Point３　：　创建圆形图像
+     * @param src
+     * @param width
+     * @return
+     */
+    public Bitmap createCircleBitmap(Bitmap src, int width) {
+
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setDither(true);
+        Bitmap resBitmap = Bitmap.createBitmap(width, width, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(resBitmap);
+        canvas.drawCircle(width / 2, width / 2, width / 2, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(src, 0, 0, paint);
+        return resBitmap;
+    }
+
+    /**
+     * Point４　：　创建圆角矩形图像
+     * @param src
+     * @param width
+     * @param height
+     * @return
+     */
+    public Bitmap createRoundCornerBitmap(Bitmap src, int width,int height) {
+
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setDither(true);
+        Bitmap resBitmap = Bitmap.createBitmap(width, width, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(resBitmap);
+        RectF rectF = new RectF(0,0,width,height);
+        canvas.drawRoundRect(rectF,mCornerRadius,mCornerRadius,paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(src, 0, 0, paint);
+        return resBitmap;
+    }
+}
